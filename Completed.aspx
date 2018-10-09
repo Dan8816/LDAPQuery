25ac008ed84143a6d6363089a45ef011e1b4bcb0
Danny Engle CTR (US) <Dengle@oai.aero>
2018-10-09 14:34:45 -0500

    1st

---
+++ Completed.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Completed.aspx.cs" Inherits="Completed" %>
+<%@ PreviousPageType VirtualPath="~/ShippingRequest.aspx" %>
+
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Shipping Request Submitted</title>
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+    <form class="form" id="form1" runat="server">
+        <h3 style="color: Maroon">OAI Shipping Request</h3>
+        <h5>The shipping request has been successfully submitted! Please print this page to send with your shipment.</h5>
+        <hr>
+            <asp:Label ID="lblCurrentDate" runat="server" Text=""></asp:Label><br />
+
+            <asp:Label ID="lblShipID" runat="server" Text=""></asp:Label><br />
+
+            <asp:Label ID="lblShipType" runat="server" Text=""></asp:Label><br />
+
+            <asp:Label ID="lblInitiatingDepartment" runat="server" Text=""></asp:Label><br /><br />
+        <hr>
+            <asp:Label ID="lblFromName" runat="server" Text=""></asp:Label><br />
+
+            <asp:Label ID="lblFromPhone" runat="server" Text=""></asp:Label><br />
+
+            <asp:Label ID="lblFromEmail" runat="server" Text=""></asp:Label><br /><br />
+        <hr>
+            <asp:Label ID="lblToName" runat="server" Text=""></asp:Label><br />
+
+            <asp:Label ID="lblToCountry" runat="server" Text=""></asp:Label><br />
+     
+            <asp:Label ID="lblToCompany" runat="server" Text=""></asp:Label><br />
+
+            <asp:Label ID="lblAddressOne" runat="server" Text=""></asp:Label><br />
+
+            <asp:Label ID="lblAddressTwo" runat="server" Text=""></asp:Label><br />
+
+            <asp:Label ID="lblToCity" runat="server" Text=""></asp:Label><br />
+
+            <asp:Label ID="lblToState" runat="server" Text=""></asp:Label><br />
+
+            <asp:Label ID="lblToPostalCode" runat="server" Text=""></asp:Label><br />
+
+            <asp:Label ID="lblToPhone" runat="server" Text=""></asp:Label><br /><br />
+        <hr>
+            <asp:GridView ID="gvItems" runat="server"></asp:GridView><br />
+        <hr>
+            <asp:Label ID="lblShipDate" runat="server" Text=""></asp:Label><br />
+
+            <asp:Label ID="lblShipTime" runat="server" Text=""></asp:Label><br /><br />
+        <hr>
+            <asp:Label ID="lblComments" runat="server" Text=""></asp:Label><br /><br />
+
+            <asp:Label ID="lblBillToThirdParty" runat="server" Text=""></asp:Label><br /><br />
+
+            <asp:Label ID="lblSignatureRequired" runat="server" Text=""></asp:Label><br /><br />
+
+            <asp:Label ID="Label1" runat="server" Text="Manager's Signature________________________________________________________________________________" Font-Bold="true"></asp:Label><br />
+
+            <asp:Label ID="Label2" runat="server" Text="Note: Package will NOT ship without signature of immediate manager."></asp:Label><br /><br />
+
+            <asp:hyperlink ID="Hyperlink1" runat="server" NavigateUrl="~/ShippingRequest.aspx">Return to Shipping Request page</asp:hyperlink>
+        </form>
+    </div>
+</body>
+</html>
---
+++ Completed.aspx.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class Completed : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var shippingDate = PreviousPage.FindControl("txtTodaysDate") as TextBox;
+        if (shippingDate != null)
+        {
+            lblCurrentDate.Text = "<b>Date:</b> " + shippingDate.Text;
+        }
+        var shipID = PreviousPage.FindControl("txtShipID") as TextBox;
+        if (shipID != null)
+        {
+            lblShipID.Text = "<b>Ship #:</b> " + shipID.Text;
+        }
+        var shipType = PreviousPage.FindControl("rblBusinessPrivate") as RadioButtonList;
+        if (shipType != null)
+        {
+            if (shipType.Text == "B")
+            {
+                lblShipType.Text = "<b>Ship Type:</b> Business";
+            }
+            else
+            {
+                lblShipType.Text = "<b>Ship Type:</b> Personal";
+            }
+        }
+        lblInitiatingDepartment.Text = "<b>Initiating Department:</b> " + GetInitiatingDepartment();
+
+        var fromName = PreviousPage.FindControl("txtFromName") as TextBox;
+        if (fromName != null)
+        {
+            lblFromName.Text = "<b>FROM:</b> " + fromName.Text;
+        }
+        var fromPhone = PreviousPage.FindControl("txtFromPhone") as TextBox;
+        if (fromPhone != null)
+        {
+           lblFromPhone.Text = "<b>Phone:</b> " + fromPhone.Text;
+        }
+        var fromEmail = PreviousPage.FindControl("txtEmailAddress") as TextBox;
+        if (fromEmail != null)
+        {
+           lblFromEmail.Text = "<b>Email:</b> " + fromEmail.Text;
+        }
+        var toName = PreviousPage.FindControl("txtToName") as TextBox;
+        if (toName != null)
+        {
+            lblToName.Text = "<b>TO:</b> " + toName.Text;
+        }
+        var toCountry = PreviousPage.FindControl("ddlCountries") as DropDownList;
+        if (toCountry != null)
+        {
+           lblToCountry.Text = "<b>Country:</b> " + toCountry.Text;
+        }
+        var toCompany = PreviousPage.FindControl("txtToCompany") as TextBox;
+        if (toCompany != null)
+        {
+           lblToCompany.Text = "<b>Company:</b> " + toCompany.Text;
+        }
+        var toAddressOne = PreviousPage.FindControl("txtToAddressOne") as TextBox;
+        if (toAddressOne != null)
+        {
+           lblAddressOne.Text = "<b>Address One:</b> " + toAddressOne.Text;
+        }
+        var toAddressTwo = PreviousPage.FindControl("txtToAddressTwo") as TextBox;
+        if (toAddressTwo != null)
+        {
+            lblAddressTwo.Text = "<b>Address Two:</b> " + toAddressTwo.Text;
+        }
+        var city = PreviousPage.FindControl("txtCity") as TextBox;
+        if (city != null)
+        {
+          lblToCity.Text = "<b>City:</b> " + city.Text;
+        }
+        //CCalk 9/18/2012 If not US then do not display state
+        var state = PreviousPage.FindControl("ddlStates") as DropDownList;
+
+        if (toCountry.Text != "US") //The country is NOT US so do not show a state
+        {
+            lblToState.Text = "<b>State:</b> ";
+        }
+        else
+        {
+            if (state != null)
+            {
+                lblToState.Text = "<b>State:</b> " + state.Text;
+            }
+        }
+        var postalCode = PreviousPage.FindControl("txtPostalCode") as TextBox;
+        if (postalCode != null)
+        {
+           lblToPostalCode.Text = "<b>Postal Code:</b> " + postalCode.Text;
+        }
+        var phone = PreviousPage.FindControl("txtToPhone") as TextBox;
+        if (phone != null)
+        {
+            lblToPhone.Text = "<b>Phone:</b> " + phone.Text;
+        }
+        //Shipping items list
+        GetShippingItems(Convert.ToInt16(shipID.Text));
+
+        var shipDate = PreviousPage.FindControl("calShipByDate") as Calendar;
+        if (shipDate != null)
+        {
+            lblShipDate.Text = "<b>Date:</b> " + shipDate.SelectedDate.ToShortDateString();
+        }
+        var shipTime = PreviousPage.FindControl("ddlShipByTime") as DropDownList;
+        if (shipTime != null)
+        {
+           lblShipTime.Text = "<b>Time:</b> " + shipTime.Text;
+        }
+        var comments = PreviousPage.FindControl("txtComments") as TextBox;
+        if (comments != null)
+        {
+           lblComments.Text = "<b>Comments:</b> " + comments.Text;
+        }
+        var billToThirdParty = PreviousPage.FindControl("txtBillTo3rdParty") as TextBox;
+        if (billToThirdParty != null)
+        {
+           lblBillToThirdParty.Text = "<b>Bill to 3rd Party:</b> " + billToThirdParty.Text;
+        }
+        var signatureRequired = PreviousPage.FindControl("cbSignatureRequired") as CheckBox;
+        if (signatureRequired != null)
+        {
+           lblSignatureRequired.Text = "<b>Signature Required:</b> " + signatureRequired.Checked;
+        }
+    }
+    private void GetShippingItems(int id)
+    {
+        //SqlConnection myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["Conn"].ConnectionString);
+
+        //const string sql = "SELECT * FROM Test_Table";
+
+        //SqlCommand myCommand = new SqlCommand(sql, myConnection);
+
+        //DataSet myDataSet = new DataSet();
+
+        //SqlDataAdapter myAdapter = new SqlDataAdapter(myCommand);
+        //myAdapter.Fill(myDataSet);
+
+        ////Bind the DataSet to the GridView
+        //gvItems.DataSource = myDataSet;
+        //gvItems.DataBind();
+
+        ////Close the connection
+        //myConnection.Close();
+
+        SqlConnection myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["OAI_ShippingRequest"].ConnectionString);
+        SqlCommand cmd = new SqlCommand();
+
+        cmd.CommandText = String.Format(@"SELECT description, quantity FROM Shipping_Items WHERE ID = {0}", id);
+        cmd.CommandType = CommandType.Text;
+        cmd.Connection = myConnection;
+
+        DataSet myDataSet = new DataSet();
+
+        SqlDataAdapter myAdapter = new SqlDataAdapter(cmd);
+        myAdapter.Fill(myDataSet);
+
+        //Bind the DataSet to the GridView
+        gvItems.DataSource = myDataSet;
+        gvItems.DataBind();
+
+        //Close the connection
+        myConnection.Close();
+    }
+    string GetInitiatingDepartment()
+    {
+        string initiatingDepartment = "";
+
+        var initiatingDepartmentPrimary = PreviousPage.FindControl("ddlInitiatingDepartment") as DropDownList;
+        var initiatingDepartmentSub = PreviousPage.FindControl("ddlSubDepartments") as DropDownList;
+        var initiatingDepartmentOther = PreviousPage.FindControl("txtOtherInitiatingDepartment") as TextBox;
+
+        string strInitiatingDepartmentPrimary = GetInitiatingDepartmentPrimary(Convert.ToInt16(initiatingDepartmentPrimary.Text));
+
+        if (strInitiatingDepartmentPrimary == "Administration" || strInitiatingDepartmentPrimary == "MX" || strInitiatingDepartmentPrimary == "Operations")
+            initiatingDepartment = GetInitiatingDepartmentSub(Convert.ToInt16(initiatingDepartmentSub.Text));
+        else if (initiatingDepartmentPrimary.Text == "Other")
+            initiatingDepartment = initiatingDepartmentOther.Text;
+        else
+            initiatingDepartment = strInitiatingDepartmentPrimary;
+
+        return initiatingDepartment;
+    }
+    private string GetInitiatingDepartmentSub(int sd)
+    {
+        SqlConnection myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["OAI_ShippingRequest"].ConnectionString);
+        SqlCommand cmd = new SqlCommand();
+        Object returnValue;
+
+        cmd.CommandText = String.Format(@"SELECT Department_Name FROM Shipping_Department WHERE Department_ID = {0}", sd);
+        cmd.CommandType = CommandType.Text;
+        cmd.Connection = myConnection;
+
+        myConnection.Open();
+
+        returnValue = cmd.ExecuteScalar();
+
+        myConnection.Close();
+
+        return returnValue.ToString();
+    }
+    private string GetInitiatingDepartmentPrimary(int pd)
+    {
+        //CCALK Pull initiating department from DB
+
+        SqlConnection myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["OAI_ShippingRequest"].ConnectionString);
+        SqlCommand cmd = new SqlCommand();
+        Object returnValue;
+
+        cmd.CommandText = String.Format(@"SELECT Department_Name FROM Shipping_Department WHERE Department_ID = {0}", pd);
+        cmd.CommandType = CommandType.Text;
+        cmd.Connection = myConnection;
+
+        myConnection.Open();
+
+        returnValue = cmd.ExecuteScalar();
+
+        myConnection.Close();
+
+        return returnValue.ToString();
+    }
+}
